562b8dd9ef966d461846b53382eecadfcac17cda
Jordan Castillo <jordantcastillo@gmail.com>
2017-12-15 18:45:39 -0800

    final tweak to stock_mkt.rb

--- stock_mkt.rb
+++ stock_mkt.rb
@@ -5,7 +5,7 @@ def stock_market(days)
 	largest_difference = 0
 
 	days.each  do|day|
-		
+
 		(day_start..day_end).each do |check_day| 
 			
 			if days[check_day].to_i > day.to_i && (days[check_day].to_i - day.to_i) > largest_difference
@@ -23,7 +23,7 @@ puts "[#{@buy_and_sell_days.join(',')}]"
 	
 end
 
-stocks = [7,13,2,5,10,3,15,9,2,10]
+stocks = [7,13,2,5,10,3,15,9,2,1]
 	
 stock_market(stocks)
 
